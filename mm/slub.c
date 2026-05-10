a241513bade40fb6238f23c72366a70f723e135c
Michal Gorlas <michal.gorlas@9elements.com>
2026-05-10 19:30:02 +0200

    mm: slab: BUG on page type confusion and wrong cache ownership
    Originally from mm: slab: BUG on page type confusion under
    BUG_ON_DATA_CORRUPTION [1], which was extracted from PaX.
    
    [1] - https://github.com/anthraxx/linux-hardened/commit/b2f5bb7312eb0e8ae18a0c84e8cd1440711b85e1
    
    Signed-off-by: Michal Gorlas <michal.gorlas@9elements.com>

--- mm/slub.c
+++ mm/slub.c
@@ -6330,13 +6330,20 @@ static noinline void warn_free_bad_obj(struct kmem_cache *s, void *obj)
 	struct slab *slab;
 
 	slab = virt_to_slab(obj);
+#ifdef CONFIG_BUG_ON_DATA_CORRUPTION
+	BUG_ON(!slab)
+#else
 	if (WARN_ONCE(!slab,
 			"kmem_cache_free(%s, %p): object is not in a slab page\n",
 			s->name, obj))
 		return;
+#endif
 
 	cachep = slab->slab_cache;
 
+#ifdef CONFIG_BUG_ON_DATA_CORRUPTION
+	BUG_ON(cachep != s)
+#else
 	if (WARN_ONCE(cachep != s,
 			"kmem_cache_free(%s, %p): object belongs to different cache %s\n",
 			s->name, obj, cachep ? cachep->name : "(NULL)")) {
@@ -6344,6 +6351,7 @@ static noinline void warn_free_bad_obj(struct kmem_cache *s, void *obj)
 			print_tracking(cachep, obj);
 		return;
 	}
+#endif
 }
 
 /**
